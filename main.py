d38108c6bc6f18ea3810f99a204491ebe0edb4fd
RNicoleti <rafael_nicoleti@live.com>
2025-03-12 21:45:54 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+screen = pygame.display.set_mode(size=(600, 480))
+print('Setup End')
+
+print('Loop Start')
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # Fechar a janela
+            quit()  # Encerrando o Pygame
